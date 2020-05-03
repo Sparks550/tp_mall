5dd63bde7c9c20aa70d2fdbf0e2a4271aff908ad
Sparks550 <1014083445@qq.com>
2020-05-04 02:59:23 +0800

    db简单练习

--- app/controller/Data.php
+++ app/controller/Data.php
@@ -3,18 +3,21 @@
 namespace app\controller;
 
 use app\BaseController;
+use think\facade\Db;
 
 class Data extends BaseController
 {
     public function index()
     {
+        //$result = Db::table("mall_demo")->where("id", 2)->find();
 
-        echo '1561588888888888888';
-
-
-        echo '15615';
-        echo 'dadad';
-
-        //try
+        //通过容器的方式处理
+        //$result2 = app("db")->table("mall_demo")->where("id", 1)->find();
+        $result = Db::table("mall_demo")
+            ->order("id","desc")
+            ->limit(1,2)
+            ->select();
+        dump($result);
+        //dump($result2);
     }
 }
