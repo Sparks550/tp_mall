0da85fdc5553cab96cfdc685280a3267712316b2
Sparks550 <1014083445@qq.com>
2020-05-18 09:40:19 +0800

    更新容易犯的错误

--- app/demo/controller/M.php
+++ app/demo/controller/M.php
@@ -1,28 +1,37 @@
 <?php
+
 namespace app\demo\controller;
 
 use app\BaseController;
 use app\Model\Demo;
 
-class M extends  BaseController{
-    public function index(){
+class M extends BaseController
+{
+    public function index()
+    {
         //获取数据表数据
-        $categoryId = $this->request->param("category_id",0,"intval");
-        if(empty($categoryId)){
-            return show(config("status.error"),"参数错误");
+        $categoryId = $this->request->param("category_id", 0, "intval");
+        if (empty($categoryId)) {
+            return show(config("status.error"), "参数错误");
         }
         $model = new Demo();
         //从url获取值
-        $results = $model->where("category_id",$categoryId)
+        $results = $model->where("category_id", $categoryId)
             ->limit(10)
-            ->order("id","desc")
+            ->order("id", "desc")
             ->select();
 
-        $cagegorys = config("category");
-        foreach($results as $key => $result){
-            $results[$key]['categoryName'] = $cagegorys[[$results["category_id"]] ?? "其他";
+            if (empty($results->toArray())) {
+            return show(config("status.success"), "数据为空");
         }
-        return show(config("status.success"),"ok",$results);
+
+        //获取config的‘category’文件信息,并对category_id做映射
+        $cagegorys = config("category");
+        foreach ($results as $key => $result) {
+            //isset($cagegorys[$result["category_id"] ? $cagegorys[$result["category_id]] : "其他";
+            $results[$key]['categoryName'] = $cagegorys[$result["category_id"]] ?? "其他";
+        }
+        return show(config("status.success"), "ok", $results);
 
     }
 }
